2c6f811096e1eb743e078a268f1b5601452f71f5
harold <haroldulrich@protonmail.com>
2023-06-12 22:58:03 -0700

    update api url

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://localhost:4000/api/v1';
+export const LOCAL_API_URL = 'http://localhost:4000/api/v1';
+export const API_URL = 'https://jwt-backend-wg2m.onrender.com/';
+
