7acc7bc2641af311b37a9ef5bbc98622afeb1657
harold <haroldulrich@protonmail.com>
2023-07-12 14:21:08 -0700

    bg mb5

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -31,7 +31,7 @@ const NavBar = ({ user, logout }) => {
   );
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-5">
+    <nav className="navbar navbar-expand-md navbar-light bg-light mb-5">
       <div className="container">
         <NavLink className="navbar-brand" to="/">Auth API</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
