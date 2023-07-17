7baeb6edd557deab58f9371d045c6d125d8e4a9b
harold <haroldulrich@protonmail.com>
2023-07-17 13:41:04 -0700

    home

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -6,7 +6,6 @@ const Home = () => {
       <h1>Home</h1>
       <h2>JWT Auth MERN</h2>
       <p>Register a new user, or Login with an existing user.</p>
-      <p>Your passwords will be encrypted on the database.</p>
     </div>
   );
 };
