9ba88bf1491f73e6ff70fe3eb998db5f229033e4
harold <haroldulrich@protonmail.com>
2023-09-06 18:23:57 -0700

    add mern background image

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
@@ -6,6 +7,7 @@ const Home = () => {
       <h1>Home</h1>
       <h2>JWT Auth MERN</h2>
       <p>Register a new user, or Login with an existing user.</p>
+      <div className='mern-home-img'></div>
     </div>
   );
 };
